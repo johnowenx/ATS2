bc3c0cfeeefd2acfe918aaf147f980dbc303e742
johnowenx <john_575@hotmail.com>
2016-03-14 22:05:07 +0000

    Removed PHP Code
    Removed PHP Database code to test deployment to AWS Elastic Beanstalk

--- search.php
+++ search.php
@@ -1,16 +1,6 @@
 <?php 
-	//Test change on git hub web
-	include 'phpscripts/header.php';
 	
-	//Get search variables
-	$term = $_GET['term'];
-	
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-   //Something Posted
-   //Further comments added to the head to test git hub
-
-}
-
+	echo phpinfo();
 
 ?>
 
@@ -21,9 +11,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<link href="../CMS/style.css" rel="stylesheet" type="text/css" />
     
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
-	<!-- Static Logo Script ( Needs JQuery at least 1.3.2 ) -->
-	<script type="text/javascript" src="http://alliedaerosystems.com/HUB/js/staticlogo.js"></script>
 </head>
 <body>
 <div class="cleaner_10"></div>
@@ -49,42 +36,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 						<input type="submit" name="search" value="Search" class="submit-button" />
 			</form>
     
-		<?php
-			//return an error messsage if no search term
-				if ($term == "") {
-					echo "<h2 style='color:black;'>Please enter a search term.</h3>";
-					echo "<p>Manufacturer and Model No are the searchable fields.</p>";
-				} else {
-					echo "<h2 style='color:black;'>Search Results for '$term':</h2>";
-
-					// We preform a bit of filtering
-					$term = strtoupper($term);
-					$term = strip_tags($term);
-					$term = trim($term);
-					
-					//aLSO INlcues
-			
-					$result = mysql_query("SELECT * FROM Manuals WHERE ((Manufacturer LIKE '%". $term ."%') OR
-														 (ModelNo LIKE '%". $term ."%'))
-														 ORDER BY Manufacturer ASC") or exit(mysql_error());
-			
-					while($row = mysql_fetch_object($result)){
-
-						//echo "<h2 style='color:black;'>$row->Manufacturer</h2>";
-						echo "<div style='font-size:18px; color:black; padding:5px 0;'>$row->Manufacturer - $row->ModelNo <a href=\"$row->FileName\"><img src=\"../CMS/images/pdfsmall.gif\" height=\"23\" width=\"23\"></a></div>";
-						//$dateConverted = $com->displayDate($row->DateUploaded);
-						echo "<div style='font-size:14px; color:black;'>$row->Description</div>";
-					}
-			//rebecca hahahahahahahahahahaha
-				}
-			echo '<div class="cleaner_20"></div>';
-			echo '<div class="cleaner_20"></div>';
-			//rebecca test
-		?>
-
 	</div>
 </div>
-<?php include 'phpscripts/footer.php';?>
 <div class="cleaner_20"></div>
 </body>
 </html>
